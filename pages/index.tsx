dd95a603814724f9b0f8dd11f7ecb6ce8d1039dc
avantgardeee <krist.andrey1@gmail.com>
2022-12-04 22:02:51 +0300

    button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import { Htag } from "../components";
+import { Htag, Button } from "../components";
 import React from "react";
 
 export default function Home(): JSX.Element {
   return (<>
     <Htag tag="h1">Текст</Htag>
+    <Button appearance='primary' arrow='right'>Кнопка</Button>
+    <Button appearance='ghost' arrow="down">Кнопка</Button>
   </>);
 }
